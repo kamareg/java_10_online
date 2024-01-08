b4e4bb5fdf541a0a9c7a0a47a34036dc3b04fa62
kamareg <kamareggg@gmail.com>
2024-01-08 13:43:24 +0200

    hw_1 ready

--- hw_1/src/main/java/ua/com/alevel/HW_1.java
+++ hw_1/src/main/java/ua/com/alevel/HW_1.java
@@ -6,5 +6,6 @@ public class HW_1 {
     public static void main(String[] args) throws IOException {
         Task1.main(new String[]{});
         Task2.main(new String[]{});
+        Task3.main(new String[]{});
     }
 }
